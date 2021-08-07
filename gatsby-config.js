c919fb9c166c8976d89858c84a61ef66f8a559f4
Marco Kühbauch <marcokuehbauch@gmail.com>
2021-08-07 13:26:57 +0200

    add prefix

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ require('dotenv').config({
 })
 
 module.exports = {
+  pathPrefix: '/faq',
   siteMetadata: {
     title: 'findingthelittlethings',
     siteUrl: 'https://findingthetlittlethings.com',
